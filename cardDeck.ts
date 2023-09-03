c25abd732f83fd354cd61b72530fd32558ace850
Ian Holloway <ian.m.holloway@gmail.com>
2023-09-02 23:59:58 -0700

    initial commit

---
+++ cardDeck.ts
@@ -0,0 +1,61 @@
+class Card {
+    constructor(public suit: string, public rank: string) { }
+
+    toString() {
+        return `${this.rank} of ${this.suit}`;
+    }
+}
+
+class Deck {
+    private cards: Card[] = [];
+
+    constructor() {
+        const suits = ["Red", "Green", "Blue", "White", "Yellow"];
+        const ranks = [
+            "handShake",
+            "handShake",
+            "handShake",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"
+        ];
+
+        for (const suit of suits) {
+            for (const rank of ranks) {
+                this.cards.push(new Card(suit, rank));
+            }
+        }
+    }
+
+    draw() {
+        if (this.cards.length === 0) {
+            throw new Error('The deck is empty');
+        }
+        return this.cards.pop() as Card;
+    }
+
+    dealHand(numCards: number): Card[] {
+        const hand: Card[] = [];
+        for (let i = 0; i < numCards; i++) {
+            hand.push(this.draw());
+        }
+        return hand;
+    }
+
+    shuffle() {
+        for (let i = this.cards.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+        }
+    }
+
+    size() {
+        return this.cards.length;
+    }
+}
