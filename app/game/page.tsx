9dee5f31397fff10ea41fe22654a2df9cfca7b9e
Ian Holloway <ian.m.holloway@gmail.com>
2023-09-07 00:37:09 -0700

    made DiscardPile component

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,11 +1,13 @@
 import CardComponent from "@/components/CardComponent";
 import CardDeck from "@/components/CardDeck";
+import DiscardPile from "@/components/DiscardPile";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between p-24">
       <CardComponent suit="Red" rank="5" />
       <CardDeck />
+      <DiscardPile empty={true} suit="Red" rank="5" />
     </main>
   );
 }
