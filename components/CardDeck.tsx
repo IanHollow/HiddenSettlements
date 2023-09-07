9576f2793ca247ff2e2a89b812e7c2acf1c0ec7a
Ian Holloway <ian.m.holloway@gmail.com>
2023-09-07 00:51:02 -0700

    simplefied CardDeck component

--- components/CardDeck.tsx
+++ components/CardDeck.tsx
@@ -3,7 +3,7 @@ export default function CardDeck() {
     <div className="rounded-lg border border-black">
       {/* h-42 = 10.5rem & w-30 = 7.5rem */}
       <div className="flex h-28 w-20 items-center justify-center md:h-[10.5rem] md:w-[7.5rem] lg:h-56 lg:w-40">
-        <div className="h-[6.25rem] w-[4.25rem] rounded-[0.25rem] bg-stone-300 md:h-[9.75rem] md:w-[6.75rem] lg:h-[13.25rem] lg:w-[9.25rem]"></div>
+        <div className="h-[90%] w-[85%] rounded-[0.25rem] bg-stone-300"></div>
       </div>
     </div>
   );
