864f4f4746bea1e8a6945a87a807065511412ac2
Ian Holloway <ian.m.holloway@gmail.com>
2023-09-09 00:13:21 -0700

    preping card deck to show multiple states

--- components/CardDeck.tsx
+++ components/CardDeck.tsx
@@ -1,6 +1,6 @@
 import CardComponent from "./CardComponent";
 
-export default function CardDeck() {
+export default function CardDeck({ empty = false }: { empty?: boolean }) {
   return (
     <CardComponent>
       <div className="h-[90%] w-[85%] rounded-[0.25rem] bg-stone-300"></div>
